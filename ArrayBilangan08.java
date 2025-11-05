70e690aefc4eadcc2febce4dca0c861260e1b55c
fareldev <ffarelscho@gmail.com>
2025-11-05 08:56:31 +0700

    Modify ArrayBilangan08

--- ArrayBilangan08.java
+++ ArrayBilangan08.java
@@ -11,5 +11,19 @@ public class ArrayBilangan08 {
         System.out.println(bil[1]);
         System.out.println(bil[2]);
         System.out.println(bil[3]);
+
+        // Modifikasi
+        int bil2[] = {5, 13, -7, 17};
+
+        System.out.println("Modifikasi Array");
+        System.out.println(bil2[0]);
+        System.out.println(bil2[1]);
+        System.out.println(bil2[2]);
+        System.out.println(bil2[3]);
+
+        System.out.println("Modifikasi Array Looping");
+        for(int i = 0; i < 4; i++) {
+            System.out.println(bil2[i]);
+        }
     }
 }
